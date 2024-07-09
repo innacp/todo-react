492d0efe08204f576ed6225b2071c64e58c850d5
innacp <inna.chapkevych@gmail.com>
2024-07-09 17:50:52 +0300

    added removing item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,42 @@
+import React, { useState } from 'react';
+
 export function TodoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+
+
+    function handleChange(e) {
+        setNewTask(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTasks([...tasks, newTask])
+        setNewTask('')
+    }
+
+    function removeTask(id){
+        setTasks(tasks.filter((task) => task.id !==id));
+    }
     
     return(
         <div>
             <form className="flex gap-[10px]" >
-                <input type="text" className="border border-slate-300 rounded-md" />
-                <button className="shadow-md bg-white rounded-md px-[10px] py-[5px]" >Add Task</button>
+                <input type="text" value={newTask} onChange={handleChange} className="outline-none border border-slate-300 rounded-md px-[5px]" />
+                <button onClick={handleSubmit} className="shadow-md bg-white rounded-md px-[10px] py-[5px]" >Add Task</button>
             </form>
             <ul>
-                <li></li>
+                {tasks.map((task) => (
+                    <li key={task}>{task}
+                    
+                        <button className="shadow-md bg-white rounded-md px-[10px] py-[5px]" 
+                        onClick={removeTask}
+                       
+                        >Delete</button>
+                    
+                    </li>
+                ))}
             </ul>
         </div>
     )
