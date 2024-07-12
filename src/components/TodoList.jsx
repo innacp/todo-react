3d543e5a76eda628ae1f369e1bfde95a904b8fcd
innacp <inna.chapkevych@gmail.com>
2024-07-12 15:03:49 +0300

    generated id

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,32 +6,35 @@ export function TodoList() {
 
 
 
-    function handleChange(e) {
-        setNewTask(e.target.value)
+    function handleChange(e) {            
+        setNewTask(e.target.value)        //passing value of an input field
     }
 
-    function handleSubmit(e) {
+    function addTask(e) {
         e.preventDefault()
-        setTasks([...tasks, newTask])
-        setNewTask('')
+        if (newTask.trim()) {
+            const taskId = Date.now() + Math.random().toString(36).substring(2);
+            setTasks([...tasks, { id: taskId, text: newTask, completed: false}])
+            setNewTask('')
+        }
     }
 
-    function removeTask(id){
-        setTasks(tasks.filter((task) => task.id !==id));
-    }
+    // function removeTask(id){
+    //     setTasks(tasks.filter((task) => task.id !==id));
+    // }
     
     return(
         <div>
             <form className="flex gap-[10px]" >
                 <input type="text" value={newTask} onChange={handleChange} className="outline-none border border-slate-300 rounded-md px-[5px]" />
-                <button onClick={handleSubmit} className="shadow-md bg-white rounded-md px-[10px] py-[5px]" >Add Task</button>
+                <button onClick={addTask} className="shadow-md bg-white rounded-md px-[10px] py-[5px]" >Add Task</button>
             </form>
             <ul>
                 {tasks.map((task) => (
-                    <li key={task}>{task}
+                    <li key={task.taskId}>{task.text}
                     
                         <button className="shadow-md bg-white rounded-md px-[10px] py-[5px]" 
-                        onClick={removeTask}
+                    
                        
                         >Delete</button>
                     
